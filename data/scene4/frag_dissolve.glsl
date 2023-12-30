5736eeabcc7505f6c8b06f33172610d9011e236e
Martijn van Iersel <mvaniersel@gmail.com>
2023-12-31 00:06:28 +0100

    Peppy: repeat dissolve effect for demo

--- data/scene4/frag_dissolve.glsl
+++ data/scene4/frag_dissolve.glsl
@@ -8,16 +8,26 @@ varying vec4 varying_color;
 varying vec2 varying_texcoord;
 void main(void)
 {
-	vec2 st = gl_FragCoord.xy;
+	// tweak for demo: slow down time and repeat.
+	float t = mod(time / 3, 2.0);
+
+	vec2 st = gl_FragCoord.xy; // actual pixel coords, not texture coords.
+
 	vec3 color = vec3(0.0);
+	
+	// checkered block size
 	float checkSize = 3.0;
 	st /= checkSize;
+
 	float step = 20.0;
 	float cellNo = mod(floor(st.x), 8.0) + 8.0 * mod(floor(st.y), 8.0);
 	float fmodResult = mod (floor(cellNo / step) + mod(cellNo, step) * step, 64.0);
-	if (fmodResult / 64.0 < time) {
+	
+	// discard blocks one by one according to pattern
+	if (fmodResult / 64.0 < t) {
 		discard;
 	}
+
 	if ( al_use_tex )
 		gl_FragColor = varying_color * texture2D( al_tex , varying_texcoord);
 	else
