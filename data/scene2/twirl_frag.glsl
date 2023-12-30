295733d93c67b5f15c977098bf21f423eda9a4ca
Martijn van Iersel <mvaniersel@gmail.com>
2023-12-31 00:25:45 +0100

    Clean up twirl shader, automatically repeat for demo

--- data/scene2/twirl_frag.glsl
+++ data/scene2/twirl_frag.glsl
@@ -7,17 +7,25 @@ precision mediump float;
 
 out vec4 vFragColor;	//fragment shader output
 
-//input from the vertex shader
-smooth in vec2 vUV;						//2D texture coordinates
+varying vec2 varying_texcoord;
 
 //shader uniforms
-uniform sampler2D textureMap;			//the image to twirl
-uniform float twirl_amount;				//teh amount of twirl
+uniform sampler2D al_tex;
+
+// uniform float twirl_amount;	//the amount of twirl
+// derived from time for demo
+uniform float time;
+
 
 void main()
 {
+   // derive twirl_amount from time for demo.
+   float twirl_amount = abs(20.0 - mod(time * 2, 40.0));
+   
+   vec2 pivot = vec2(0.5);
+
 	//get the shifted UV coordinates so that the origin of twirl is at the center of image
-	vec2 uv = vUV-0.5;
+	vec2 uv = varying_texcoord-pivot;
 	
 	//get the angle from the shifter UV coordinates
    float angle = atan(uv.y, uv.x);
@@ -26,11 +34,15 @@ void main()
    float radius = length(uv);
 
    //increment angle by product of twirl amount and radius
-   angle+= radius*twirl_amount; 
+   angle += radius * twirl_amount; 
 
    //convert to Cartesian coordinates
-   vec2 shifted = radius* vec2(cos(angle), sin(angle));
+   vec2 shifted = radius* vec2(cos(angle), sin(angle)) + pivot;
+
+   if (shifted.x < 0 || shifted.y < 0 || shifted.x > 1 || shifted.y > 1) {
+      discard;
+   }
 
    //shift by 0.5 to bring it back to original unshifted position
-   vFragColor = texture(textureMap, (shifted+0.5));    
+   vFragColor = texture(al_tex, (shifted));    
 }
