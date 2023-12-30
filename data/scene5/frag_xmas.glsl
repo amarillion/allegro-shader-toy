a2844c646f0cb4da0908c322ad8467acd4fb7ed6
Martijn van Iersel <mvaniersel@gmail.com>
2023-12-30 16:55:00 +0100

    Xmas shader improvements

--- data/scene5/frag_xmas.glsl
+++ data/scene5/frag_xmas.glsl
@@ -9,39 +9,50 @@ uniform bool al_use_tex;
 
 void main(void)
 {
-	vec2 st = gl_FragCoord.xy;
-	vec3 color = vec3(0.0);
-	float pixelSize = 4.0;
-	st /= pixelSize;
-	float step = 20.0;
-	vec2 cellco = mod(floor(st), 8.0);
-	
-	if (varying_color.a == 0) {
-		discard;
+	// fallback for non-texture use
+	if (!al_use_tex) {
+		gl_FragColor = varying_color;
+		return;	
 	}
 
+	// check if we're outside texture
+	if (texture2D(al_tex, varying_texcoord).a == 0) discard;
+
+	ivec2 ts = textureSize(al_tex, 0);
+	vec2 pix = 1.0 / ts;
+
+	vec2 st = gl_FragCoord.xy;
+	st /= 80;
+	
+	float diag = st.x - st.y - (time * 1.3);
+	float diag2 = st.x + st.y + (time * 0.5);
+
 	vec4 xmas;
-	if (cellco.x < cellco.y + mod(time * 12.0, 8.0)) {
-		xmas = vec4(1, 0, 0, 1);
+	if (mod(diag, 2) > 1) {
+		float intensity = abs(1.0 - mod(diag2 / 1.0, 2.0));
+		xmas = vec4(intensity, 0, 0, 1);
 	}
 	else {
-		xmas = vec4(0, 1, 0, 1);
+		float intensity = abs(1.0 - mod((diag2 + 1.0) / 1.0, 2.0));
+		xmas = vec4(0, intensity, 0, 1);
 	}
 
 	if ( al_use_tex ) {
 
 		if (texture2D( al_tex , varying_texcoord).a == 0) discard;
 
-		// sample surrounding pixels
-		float dist = 0.001;
-		if (texture2D( al_tex , varying_texcoord + vec2(dist, 0)).a == 0 ||
-			texture2D( al_tex , varying_texcoord + vec2(0, dist)).a == 0 ||
-			texture2D( al_tex , varying_texcoord + vec2(-dist, 0)).a == 0 ||
-			texture2D( al_tex , varying_texcoord + vec2(0, -dist)).a == 0
-		) {
-			xmas = vec4(1, 1, 1, 1);
+		for (int j = 1; j < 4; ++j) {
+			if (
+				texture2D( al_tex , varying_texcoord + vec2(pix.x*j, 0)).a == 0 ||
+				texture2D( al_tex , varying_texcoord + vec2(0, pix.y*j)).a == 0 ||
+				texture2D( al_tex , varying_texcoord + vec2(-pix.x*j, 0)).a == 0 ||
+				texture2D( al_tex , varying_texcoord + vec2(0, -pix.y*j)).a == 0
+			) {
+					xmas = vec4(1, 1, 1, 1);
+					break;
+			}		
 		}
-		
+
 	}
 	
 	gl_FragColor = xmas;
